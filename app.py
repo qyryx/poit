4e9fd726b930c7ee866488c6e98138691a14f102
qyryx <xhornika@stuba.sk>
2024-06-04 23:56:57 +0200

    added logging to DB and file, finished app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # import Adafruit_DHT
 from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 import time
 import random
 import threading
+import redis
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -11,15 +12,39 @@ is_streaming = True
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+stop_event = threading.Event()
+bg_thread = None
+
+data_log_file = 'data.log'
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+def write_to_data_log(data):
+    with open(data_log_file, 'a') as file:
+        file.write(f"Time: {time.strftime('%Y-%m-%d %H:%M:%S')} Temperature: {data['temperature']}°C Humidity: {data['humidity']}%\n")
+
+
+def write_to_db(data):
+    log_message = f"Time: {time.strftime('%Y-%m-%d %H:%M:%S')} Temperature: {data['temperature']}°C Humidity: {data['humidity']}%"
+    redis_client.rpush('data_log', log_message)
 
 
 def background_task():
-    while True:
-        temperature = random.uniform(0, 40)
-        humidity = random.uniform(0, 100)
+    while not stop_event.is_set():
+        temperature = random.uniform(15, 25)
+        humidity = random.uniform(40, 50)
         data = {'temperature': round(temperature, 1), 'humidity': round(humidity, 1)}
+        write_to_data_log(data)
+        write_to_db(data)
         socketio.emit('data_update', data, namespace='/data')
-        time.sleep(1)
+        for _ in range(10):
+            if stop_event.is_set():
+                break
+            time.sleep(0.1)
+        #     sensor = Adafruit_DHT.DHT11
+        #     pin = 4
+        #     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        #     return jsonify(temperature=temperature, humidity=humidity)
 
 
 @app.route('/')
@@ -33,7 +58,7 @@ def index():
 
 
 @app.route('/gauge')
-def logging():
+def gauge():
     return render_template('gauge.html')
 
 
@@ -42,49 +67,66 @@ def graph():
     return render_template('graph.html')
 
 
+@app.route('/file')
+def file():
+    return render_template('file.html')
+
+
+@app.route('/db')
+def db():
+    return render_template('db.html')
+
+
+def read_data_log():
+    data_list = []
+    with open(data_log_file, 'r') as file:
+        for line in file:
+            data_list.append(line.strip())
+    return data_list
+
+
+@app.route('/from-file')
+def get_data():
+    data_list = read_data_log()
+    return jsonify(data_list)
+
+
+@app.route('/database')
+def database():
+    data_logs = redis_client.lrange('data_log', 0, -1)
+    return jsonify([log.decode('utf-8') for log in data_logs])
+
+
 @app.route('/start', methods=['POST'])
 def start_stream():
-    global is_streaming
-    is_streaming = True
-    return jsonify({"status": "started"})
+    global bg_thread
+    if bg_thread is None or not bg_thread.is_alive():
+        stop_event.clear()
+        bg_thread = threading.Thread(target=background_task)
+        bg_thread.daemon = True
+        bg_thread.start()
+        return jsonify({"status": "started"})
+    else:
+        return jsonify({"status": "already running"})
 
 
 @app.route('/stop', methods=['POST'])
 def stop_stream():
-    global is_streaming
-    is_streaming = False
+    stop_event.set()
+    if bg_thread is not None:
+        bg_thread.join()
     return jsonify({"status": "stopped"})
-
-
-@app.route('/metrics')
-def metrics():
-    global is_streaming
-    if is_streaming:
-        temperature = random.uniform(10, 30)
-        humidity = random.uniform(10, 100)
-        return jsonify(temperature=temperature, humidity=humidity)
-    # global is_streaming
-    # if is_streaming:
-    #     sensor = Adafruit_DHT.DHT11
-    #     pin = 4
-    #     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    #     return jsonify(temperature=temperature, humidity=humidity)
-    # else:
-    #     return jsonify(temperature=None, humidity=None)
 
 
 @socketio.on('connect', namespace='/data')
 def handle_connect():
-    print('Client connected')
+    print("Client connected")
 
 
 @socketio.on('disconnect', namespace='/data')
 def handle_disconnect():
-    print('Client disconnected')
+    print("Client disconnected")
 
 
 if __name__ == '__main__':
-    bg_thread = threading.Thread(target=background_task)
-    bg_thread.daemon = True
-    bg_thread.start()
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
